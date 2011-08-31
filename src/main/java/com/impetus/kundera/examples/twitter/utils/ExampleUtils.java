643f662d0ad440ae49c91bc6c7bb0117adeb20f7
amry.ks@gmail.com <amresh.singh@impetus-d189.impetus.co.in>
2011-08-31 12:45:12 +0530

    cleanup

--- src/main/java/com/impetus/kundera/examples/twitter/utils/ExampleUtils.java
+++ src/main/java/com/impetus/kundera/examples/twitter/utils/ExampleUtils.java
@@ -35,9 +35,9 @@ public class ExampleUtils
         return new Date().getTime();
     }
 
-    public static void main(String[] args)
+    /*public static void main(String[] args)
     {
         System.out.println(getCurrentTimestamp());
-    }
+    }*/
 
 }
