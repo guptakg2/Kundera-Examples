9b0f9bee757e78a34e2d91df30cfc1edbac1a8ca
vishri <vivek_shri@yahoo.com>
2011-09-26 18:13:41 +0530

    Removed Un-necessary Property Files

--- src/main/java/com/impetus/kundera/examples/twitter/entities/Preference.java
+++ src/main/java/com/impetus/kundera/examples/twitter/entities/Preference.java
@@ -29,7 +29,7 @@ import com.impetus.kundera.examples.twitter.utils.ExampleUtils;
  */
 
 @Entity
-@Table(name = "preference", schema = "KunderaExamples@twibase")
+@Table(name = "preference", schema = "KunderaExamples@twissandra")
 public class Preference
 {
     @Id
